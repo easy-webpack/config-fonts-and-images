4b9708875ea18225703962627b472519d6a30927
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2016-11-08 17:34:20 +0100

    feat(index): add -loader suffix
    done in preparation for https://github.com/webpack/webpack/pull/3102

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,10 @@ export = function fontsAndImages() {
       module: {
         rules: get(this, 'module.rules', []).concat([
           // embed small images and fonts as Data Urls and larger ones as files
-          { test: /\.(png|gif|jpg)$/, loader: 'url', query: { limit: 8192 } },
-          { test: /\.woff2(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url', query: { limit: 10000, mimetype: 'application/font-woff2' } },
-          { test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url', query: { limit: 10000, mimetype: 'application/font-woff' } },
-          { test: /\.(ttf|eot|svg|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file' },
+          { test: /\.(png|gif|jpg)$/, loader: 'url-loader', query: { limit: 8192 } },
+          { test: /\.woff2(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader', query: { limit: 10000, mimetype: 'application/font-woff2' } },
+          { test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader', query: { limit: 10000, mimetype: 'application/font-woff' } },
+          { test: /\.(ttf|eot|svg|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' },
         ])
       }
     }
